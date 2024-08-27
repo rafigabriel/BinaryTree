f509f8b8a7c7e6e224d15aead02b7e599e7ef8fc
Rafi Gabriel <147555432+rafigabriel@users.noreply.github.com>
2024-08-27 17:20:56 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,11 +1,23 @@
 public class App {
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
-        tree.insert((new Node(30)));
-        tree.insert((new Node(10)));
-        tree.insert((new Node(20)));
-        tree.insert((new Node(40)));
+        tree.insert(new Node(30));
+        tree.insert(new Node(10));
+        tree.insert(new Node(5));
+        tree.insert(new Node(20));
+        tree.insert(new Node(15));
+        tree.insert(new Node(13));
+        tree.insert(new Node(12));
+        tree.insert(new Node(40));
+        tree.insert(new Node(50));
+        tree.insert(new Node(60));
 
-        tree.preOrder();
+        tree.inOrder();
+        System.out.println("---------------------------------------");
+        tree.remove(5);
+        tree.inOrder();
+        System.out.println("---------------------------------------");
+        tree.remove(50);
+        tree.inOrder();
     }
 }
